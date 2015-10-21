c00ee0f3ef17f6b2ddaff5eb1c2b716517b39b23
Paul Heasley <paul.heasley@gmail.com>
2015-10-22 08:40:44 +1100

    Simple static site

---
+++ js/listmenuh.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 9:40:26 Feb 27, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:55:52 Oct 21, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+sfHover = function() {
+	var sfEls = document.getElementById("primary-nav").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
